0cbe1943045ce3ce384283356994acfc962d1b43
Joel Tan <joelypolly@gmail.com>
2025-01-16 11:16:38 -0800

    Functional Code that moves the elements around

--- app/server.py
+++ app/server.py
@@ -28,7 +28,7 @@ def init_db():
 @app.route('/')
 def index():
     conn = get_db_connection()
-    todos = conn.execute('SELECT * FROM todo').fetchall()
+    todos = conn.execute('SELECT * FROM todo ORDER BY position ASC').fetchall()
     conn.close()
     return render_template('index.html', todos=todos)
 
@@ -40,13 +40,52 @@ def add():
         # Alternative is to update the frontend to support JSON request i.e. AJAX 
         # or fetch with custom payload
         conn = get_db_connection()
-        conn.execute('INSERT INTO todo (title) VALUES (?)', (title,))
+        latestTodo = conn.execute('SELECT * FROM todo ORDER BY position DESC LIMIT 1').fetchone()
+        # New inserts will + 1 to the last one. Not really ideal in multithreaded applications but
+        # will do for a small sample app
+        position = 1 if latestTodo is None else latestTodo['position'] + 1
+        conn.execute('INSERT INTO todo (title, position) VALUES (?, ?)', (title, position))
         conn.commit()
         conn.close()
         return redirect(url_for('index'))
 
     return render_template('add.html')
 
+@app.route('/move/<int:id>/<string:direction>', methods=(['GET']))
+def move(id, direction):
+    conn = get_db_connection()
+    todo = conn.execute('SELECT * FROM todo WHERE id = ?', (id,)).fetchone()
+    
+    if todo is None:
+        conn.close()
+        return redirect(url_for('index'))
+    
+    if direction == 'up':
+        # Get the previous todo by position
+        previousTodo = conn.execute('SELECT * FROM todo WHERE position < ? ORDER BY position DESC LIMIT 1', (todo['position'],)).fetchone()
+        if (previousTodo is None): # If there is no previous todo, do nothing
+            conn.close()
+            return redirect(url_for('index'))
+        conn.execute('BEGIN TRANSACTION')
+        conn.execute('UPDATE todo SET position = ? WHERE id = ?', (todo['position'], previousTodo['id']))
+        conn.execute('UPDATE todo SET position = ? WHERE id = ?', (previousTodo['position'], todo['id']))
+        conn.commit()
+    elif direction == 'down':
+        # Get the next todo by position
+        nextTodo = conn.execute('SELECT * FROM todo WHERE position > ? ORDER BY position ASC LIMIT 1', (todo['position'],)).fetchone()
+        if (nextTodo is None): # If there is no next todo, do nothing
+            conn.close()
+            return redirect(url_for('index'))
+        conn.execute('BEGIN TRANSACTION')
+        conn.execute('UPDATE todo SET position = ? WHERE id = ?', (todo['position'], nextTodo['id']))
+        conn.execute('UPDATE todo SET position = ? WHERE id = ?', (nextTodo['position'], todo['id']))
+        conn.commit()
+    else:
+        conn.close()
+        return redirect(url_for('index'))
+
+    conn.close()
+    return redirect(url_for('index'))
 
 @app.route('/delete/<int:id>')
 def delete(id):
